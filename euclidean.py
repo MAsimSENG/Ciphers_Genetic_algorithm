1a51d3ab0daaef56618090db04ba764a59a5c3f7
Asim <maali@uvic.ca>
2019-07-06 17:34:35 -0700

    fitness function done

--- euclidean.py
+++ euclidean.py
@@ -1,30 +1,54 @@
 
 #!/usr/bin/env python3
- 
- 
-import os
-from python_speech_features import mfcc
-import librosa as li
-import numpy as np
+
 from math import*
-from decimal import Decimal
+import numpy as np
+
+"""A function to return the euclidean distance between two strings
+
+    ## the longest distance from one point to another is 13 (ie a-n)
+    ## Consider the example for forward distance larger than 13 :
+    ex:  a-o = 14,
+    ## 14>13 --> how_much_back = 14-13 =1
+    ## back_distance = 13 - how_much_back = 12
+
+    Parameters:
+    (string) knownText
+    (string) decryptedText
+    (int ) distanceArray[]
+
+"""
+def euclideanDistance(knownText, decryptedText):
+    distanceArray =[]
 
 
+    for knownLetter,decryptedLetter in zip(knownText,decryptedText):
 
- 
+        forward_dist = abs(ord(knownLetter) - ord(decryptedLetter))
 
-def euclidean_distance(x,y):
-    
- 
-    return sqrt(sum(pow(a-b,2) for a, b in zip(x, y)))
+        if(forward_dist > 13):
 
- 
+            how_much_back = forward_dist - 13
+
+            back_distance = 13 - how_much_back
+
+            distanceArray.append(back_distance)
+
+        else:
+
+            distanceArray.append(forward_dist)
+
+
+    distanceArray = np.array(distanceArray)
+
+    return  np.sqrt(np.sum(np.square(distanceArray)))
+
+
 def main():
- 
-    """ the main function to create Similarity class instance and get used to it """
- 
- 
-    print (euclidean_distance(["a","b","c","d"],["a","b","c","d"]))
-    
+
+    distance = euclideanDistance("bbcz","abcd")
+
+    print(distance)
+
 if __name__ == "__main__":
     main()
