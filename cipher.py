8c274f6e6bd72f332ec385f2ab393da018394ddf
Rafay Chaudhry <rafay_chaudhry@live.com>
2019-07-07 19:17:35 -0700

    Added full functionality

--- cipher.py
+++ cipher.py
@@ -1,26 +1,118 @@
 #!/user/bin/env python3
-
+import itertools
 # Constant
 LETTER_SHIFT = 97
 
-def simpleShiftEncrypt(string, shift):
+def decrypt(text, key):
+	'''
+	This method determines the type of key (either int, string or list) and calls the necessary function to decrypt the text.
+	'''
+	if(type(key) == int):
+		intDecrypt(text, key)
+	elif(type(key) == str):
+		strDecrypt(text,key)
+	elif(type(key) == list):
+		intListDecrypt(text, key)
+	else:
+		print ('Not supported')
+
+def encrypt(text, key):
+	'''
+	This method determines the type of key (either int, string or list) and calls the necessary function to encrypt the text.
+	'''
+	if(type(key) == int):
+		intEncrypt(text, key)
+	elif(type(key) == str):
+		strEncrypt(text,key)
+	elif(type(key) == list):
+		intListEncrypt(text, key)
+	else:
+		print ('Not supported')
+
+def strDecrypt(text, key):
+	'''
+	This method encrypts (shifts) the text by iterating through a string. For this method when the chars are converted into ints, they are increased by +1 to convert a=0 to a=1, b=1 to b=2...(This is only done when LETTER_SHIFT = 97)
+	'''
+	intKeyList = []
+	for letter, i in zip(key, itertools.count(0,1)):
+		intKeyList.append(charToIntASCII(letter) + 1)
+	intListDecrypt(text,intKeyList)
+
+def strEncrypt(text, key):
+	'''
+	This method encrypts (shifts) the text by iterating through a string. For this method when the chars are converted into ints, they are increased by +1 to convert a=0 to a=1, b=1 to b=2...(This is only done when LETTER_SHIFT = 97)
+	'''
+	intKeyList = []
+	for letter, i in zip(key, itertools.count(0,1)):
+		intKeyList.append(charToIntASCII(letter) + 1)
+	intListEncrypt(text,intKeyList)
+
+def intListDecrypt(text, key):
+	'''
+	This method decrypts (shifts) the text by iterating through an integer list
+	'''
+	decrypted = ""
+	for letter, shift in zip(text, itertools.cycle(key)):
+		decrypted += decryptShiftLetter(letter,shift)
+	print(text) #TODO Remove this line
+	print(decrypted) #TODO Remove this line
+
+def intListEncrypt(text, key):
+	'''
+	This method encrypts (shifts) the text by iterating through an integer list
+	'''
 	encrypted = ""
-	for letter in string:
-		letter = charToIntASCII(letter)
-		letter += shift
-		encrypted += intToCharASCII(letter)
-	print(string)
-	print(encrypted)
+	for letter, shift in zip(text, itertools.cycle(key)):
+		encrypted += encryptShiftLetter(letter,shift)
+	print(text) #TODO Remove this line
+	print(encrypted) #TODO Remove this line
+
+def intDecrypt(text, key):
+	'''
+	This method decrypts (shifts) the text by a constant amount for each letter
+	'''
+	shift = key
+	decrypted = ""
+	for letter in text:
+		decrypted += decryptShiftLetter(letter,shift)
+	print(text) #TODO Remove this line
+	print(decrypted) #TODO Remove this line
+
+def intEncrypt(text, key):
+	'''
+	This method encrypts (shifts) the text by a constant amount for each letter
+	'''
+	shift = key
+	encrypted = ""
+	for letter in text:
+		encrypted += encryptShiftLetter(letter,shift)
+	print(text) #TODO Remove this line
+	print(encrypted) #TODO Remove this line
+
+def decryptShiftLetter(letter, shift):
+	'''
+	Shifts a letter by the specified amount
+	'''
+	letter = charToIntASCII(letter)
+	letter -= shift
+	letter = intToCharASCII(letter)
+	return letter
+
+def encryptShiftLetter(letter, shift):
+	'''
+	Shifts a letter by the specified amount
+	'''
+	letter = charToIntASCII(letter)
+	letter += shift
+	letter = intToCharASCII(letter)
+	return letter
 
 def charToIntASCII(letter):
 	'''
 	Shift letters to a=0, b=1, c=2 ... z=25 if LETTER_SHIFT == 97
-	Shift letters to a=1, b=2, c=3 ... z=26 if LETTER_SHIFT == 96
 	returns a number
 	'''
-
 	return ord(letter) - LETTER_SHIFT
-
 
 def intToCharASCII(letter):
 	'''
@@ -30,16 +122,8 @@ def intToCharASCII(letter):
 	'''
 	return chr((letter % 26) + LETTER_SHIFT)
 
-# def intToCharASCII(letter):
-# 	'''
-# 	Only use if LETTER_SHIFT == 96
-# 	Takes an integer and returns a character
-# 	Shift letters a=97,b=98,c=99...z=122
-# 	'''
-# 	if(letter % 26 == 0):
-# 		return 'a'
-# 	else:
-# 		return chr((letter % 26) + LETTER_SHIFT)
-
 if __name__ == '__main__':
-    simpleShiftEncrypt('abcdz',27)
+	# encrypt('abcdef','abc')
+	# encrypt('abcdef',[1,2,3])
+	encrypt('xyz','abc')
+	decrypt('yac','abc')
